ce77b00fd993ed1c594f68b5de8cb4555f81cd46
Nate Willis <natewillis1@gmail.com>
2020-07-24 15:27:19 -0500

    added allowed hosts to django-environ

--- hyperion/hyperion/settings.py
+++ hyperion/hyperion/settings.py
@@ -39,7 +39,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = tuple(env.list('ALLOWED_HOSTS', default=[]))
 
 # Application definition
 
